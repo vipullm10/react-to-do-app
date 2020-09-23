60b0a397ddbc33fbaf5dbe6c50ab43d50211faf3
Vipul Anil Devnani <vipuldevnani97@gmail.com>
2020-09-24 01:59:06 +0530

    Updates

---
+++ precache-manifest.403d0f53dededc5d7bfa44f00e09df4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a00e46f6ceea354815461078b7ae0e",
+    "url": "/react-to-do-app/index.html"
+  },
+  {
+    "revision": "398b380761177536dea3",
+    "url": "/react-to-do-app/static/css/main.6ace192d.chunk.css"
+  },
+  {
+    "revision": "fa5172cd0252be71c1ed",
+    "url": "/react-to-do-app/static/js/2.5177670a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do-app/static/js/2.5177670a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398b380761177536dea3",
+    "url": "/react-to-do-app/static/js/main.0ef450dd.chunk.js"
+  },
+  {
+    "revision": "4561a864b00d8d3dd97f",
+    "url": "/react-to-do-app/static/js/runtime-main.6bb5bc56.js"
+  },
+  {
+    "revision": "0d10f8f10c38b84eada450482ad46fb4",
+    "url": "/react-to-do-app/static/media/trash.0d10f8f1.svg"
+  }
+]);
